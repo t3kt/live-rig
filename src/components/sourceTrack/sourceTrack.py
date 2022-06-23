2c48da11c3f2a108db64eafe8ab3d02e72deec3e
t3kt <tekt@immerse.studio>
2022-06-23 11:35:48 -0700

    add clear and edit buttons in sourceTrack

--- src/components/sourceTrack/sourceTrack.py
+++ src/components/sourceTrack/sourceTrack.py
@@ -1,3 +1,4 @@
+from liveCommon import navigateTo
 import logging
 from typing import Optional
 
@@ -24,6 +25,7 @@ _logger = logging.getLogger(__name__)
 class SourceTrack:
 	def __init__(self, ownerComp: '_Comp'):
 		self.ownerComp = ownerComp
+		self.sceneInfo = ownerComp.op('sceneInfo')  # type: DAT
 
 	def OnStartup(self):
 		pass
@@ -32,5 +34,16 @@ class SourceTrack:
 		_logger.info(f'LoadScene({name!r})')
 		self.ownerComp.par.Scenename = name or ''
 
+	def UnloadScene(self):
+		self.LoadScene(None)
+
 	def GetSceneName(self):
 		return self.ownerComp.par.Scenename.eval()
+
+	def GetSceneComp(self):
+		return op(self.sceneInfo[1, 'comp'])
+
+	def EditScene(self):
+		comp = self.GetSceneComp()
+		if comp:
+			navigateTo(comp, popup=ui.performMode)
--- src/lib/liveCommon.py
+++ src/lib/liveCommon.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Optional, Union
 
 # noinspection PyUnreachableCode
 if False:
@@ -12,3 +12,42 @@ def queueCall(action: Callable, args: list = None, delayFrames=5, delayRef=None)
 		'args[0](*(args[1:]))',
 		action, *(args or []),
 		delayFrames=delayFrames, delayRef=delayRef or root)
+
+def getActiveEditor() -> 'NetworkEditor':
+	pane = ui.panes.current
+	if pane and pane.type == PaneType.NETWORKEDITOR:
+		return pane
+	for pane in ui.panes:
+		if pane.type == PaneType.NETWORKEDITOR:
+			return pane
+
+def _getPaneByName(name: str):
+	for pane in ui.panes:
+		if pane.name == name:
+			return pane
+
+def _getEditorPane(name: Optional[str] = None, popup=False):
+	if name:
+		pane = _getPaneByName(name)
+	else:
+		pane = getActiveEditor()
+	if pane:
+		if popup:
+			return pane.floatingCopy()
+		return pane
+	else:
+		return ui.panes.createFloating(type=PaneType.NETWORKEDITOR, name=name)
+
+def navigateTo(o: 'Union[OP, COMP]', name: Optional[str] = None, popup=False, goInto=True):
+	if not o:
+		return
+	pane = _getEditorPane(name, popup)
+	if not pane:
+		return
+	if goInto and o.isCOMP:
+		pane.owner = o
+	else:
+		pane.owner = o.parent()
+		o.current = True
+		o.selected = True
+		pane.homeSelected(zoom=False)
